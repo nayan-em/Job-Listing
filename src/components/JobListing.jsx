eddbc47bdcdcd3fb6d97b8ee5bf9a3cd93b4a084
Nayan Sharma <nayan@Nayans-MacBook-Air.local>
2024-12-01 12:46:30 +0530

    Fetch data from json server using useEffect hook

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,8 +1,25 @@
+import { useState, useEffect } from 'react';
 import Job from './Job'
-import {jobs} from '../assets/json/jobs.json'
 
 const JobListing = ({ isHome = false }) => {
-  const jobListings = isHome ? jobs.slice(0, 3) : jobs;
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  // Whenever second parameter changes, the func is run
+  useEffect(() => { 
+    const fetchJobs = async () => {
+      try {
+        const res = await fetch('http://localhost:8000/jobs');
+        const data = await res.json();
+        setJobs(data);
+      } catch (erorr) {
+        console.log("Error fetching data", error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchJobs();
+  }, [])
 
   return (
     <section className="bg-blue-50 px-4 py-10">
@@ -11,7 +28,7 @@ const JobListing = ({ isHome = false }) => {
           { isHome ? 'Recent Jobs' : 'Browse Jobs' }
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {jobListings.map((job) => (
+          {jobs.map((job) => (
             <Job key={job.id} job={job}/>
           ))}
         </div>
