7b8e2e5e085e4dbe35ac340312b0044424a85c6a
Nayan Sharma <nayan@Nayans-MacBook-Air.local>
2024-12-01 13:01:57 +0530

    Add spinner for loading animation

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,15 +1,16 @@
-import { useState, useEffect } from 'react';
-import Job from './Job'
+import { useState, useEffect } from "react";
+import Job from "./Job";
+import Spinner from "./Spinner";
 
 const JobListing = ({ isHome = false }) => {
   const [jobs, setJobs] = useState([]);
   const [loading, setLoading] = useState(true);
 
   // Whenever second parameter changes, the func is run
-  useEffect(() => { 
+  useEffect(() => {
     const fetchJobs = async () => {
       try {
-        const res = await fetch('http://localhost:8000/jobs');
+        const res = await fetch("http://localhost:8000/jobs");
         const data = await res.json();
         setJobs(data);
       } catch (erorr) {
@@ -17,26 +18,28 @@ const JobListing = ({ isHome = false }) => {
       } finally {
         setLoading(false);
       }
-    }
+    };
     fetchJobs();
-  }, [])
+  }, []);
 
   return (
     <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
         <h2 className="text-3xl font-bold text-indigo-500 mb-10 text-center">
-          { isHome ? 'Recent Jobs' : 'Browse Jobs' }
+          {isHome ? "Recent Jobs" : "Browse Jobs"}
         </h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {loading ? (<h2>Loading...</h2>) : (
-            jobs.map((job) => (
-              <Job key={job.id} job={job}/>
-            ))
-          )}
-        </div>
+        {loading ? (
+          <Spinner loading={loading} />
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {jobs.map((job) => (
+              <Job key={job.id} job={job} />
+            ))}
+          </div>
+        )}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default JobListing
+export default JobListing;
